b6263f93ac547156424403a9c48cb88de48494d3
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2025-04-11 15:53:52 -0700

    WIP

--- kubernetes/delete.sh
+++ kubernetes/delete.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -z "$KUBECONFIG" ]; then
-    kubectl delete -f kubernetes/twoge.configmap.yaml --kubeconfig $KUBECONFIG --ignore-not-found
-    kubectl delete -f kubernetes/twoge.secret.yaml --kubeconfig $KUBECONFIG --ignore-not-found
-    kubectl delete -f kubernetes/twoge.deployment.yaml --kubeconfig $KUBECONFIG --ignore-not-found
-    kubectl delete -f kubernetes/postgres.deployment.yaml --kubeconfig $KUBECONFIG --ignore-not-found
-    kubectl delete -f kubernetes/postgres.pvc.yaml --kubeconfig $KUBECONFIG --ignore-not-found
+    kubectl delete -f kubernetes/twoge.configmap.yaml --ignore-not-found --kubeconfig $KUBECONFIG
+    kubectl delete -f kubernetes/twoge.secret.yaml --ignore-not-found --kubeconfig $KUBECONFIG
+    kubectl delete -f kubernetes/twoge.deployment.yaml --ignore-not-found --kubeconfig $KUBECONFIG
+    kubectl delete -f kubernetes/postgres.deployment.yaml --ignore-not-found --kubeconfig $KUBECONFIG
+    kubectl delete -f kubernetes/postgres.pvc.yaml --ignore-not-found --kubeconfig $KUBECONFIG
 else
     kubectl delete -f kubernetes/twoge.configmap.yaml
     kubectl delete -f kubernetes/twoge.secret.yaml
